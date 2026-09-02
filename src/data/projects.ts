68854c02e9ceda1638f3171923537f94170426dc
KajaJohanne <kajajohanne@gmail.com>
2026-09-02 15:54:31 +0200

    prosjekter: fikset layout med placeholder bilder

---
+++ src/data/projects.ts
@@ -0,0 +1,21 @@
+export const projects = [
+  {
+    id: "strikkeapp",
+    title: "Strikkemønster-editor",
+    description: "Et rutenett-verktøy for å designe strikkediagram.",
+    image: "/images/placeholder.svg",
+  },
+  {
+    id: "tekstapp",
+    title: "Tekstapp",
+    description:
+      "Bachelorprosjekt i samarbeid med Statens vegvesen. Program for å opprette og håndtere tekstnøkler.",
+    image: "images/placeholder.svg",
+  },
+  {
+    id: "portfolio",
+    title: "Denne siden",
+    description: "Portefølje bygget med React, Tailwind og React Router",
+    image: "/images/placeholder.svg",
+  },
+];
