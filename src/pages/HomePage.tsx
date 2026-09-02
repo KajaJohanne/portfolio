05cb07c4ffaad1829f8e1228813fde4f31f507fc
KajaJohanne <kajajohanne@gmail.com>
2026-09-02 14:22:00 +0200

    fikset struktur til hero med navn, bilde og test, placeholder info

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,9 @@
+import { Hero } from "../components/Hero";
+
 export function HomePage() {
   return (
     <div>
-      <h1>heia</h1>
-      <section id="hjem">her er hjemdelen</section>
+      <Hero />
       <section id="om-meg">her er om meg delen</section>
       <section id="prosjekter">her er prosjekter delen</section>
     </div>
