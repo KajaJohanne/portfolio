97a014be1c72ba8c421490e0630388eb8e827051
KajaJohanne <kajajohanne@gmail.com>
2026-09-11 19:03:40 +0200

    fikset router basename for github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Layout } from './components/Layout'
-import { HomePage } from "./pages/HomePage"; 
-import { ProjectPage } from './pages/ProjectPage';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Layout } from "./components/Layout";
+import { HomePage } from "./pages/HomePage";
+import { ProjectPage } from "./pages/ProjectPage";
 
 function App() {
-  
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route element={<Layout />}>
-      <Route index element={<HomePage />}/>
-      <Route path="prosjekter/:id" element={<ProjectPage />}/>
-      </Route>
-    </Routes>
+    <BrowserRouter basename="/portfolio">
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="prosjekter/:id" element={<ProjectPage />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
